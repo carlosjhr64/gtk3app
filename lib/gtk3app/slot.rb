64880dbacf90a0c3c276d975d232f814ab0d2a12
carlosjhr64 <carlosjhr64@gmail.com>
2014-11-23 13:47:28 -0800

    Slot module

---
+++ lib/gtk3app/slot.rb
@@ -0,0 +1,31 @@
+module Gtk3App
+module Slot
+  using Rafini::Array
+
+  def self.dbm
+    SDBM.open(Gtk3App::CONFIG[:SLOTS_DBM]){|db|yield(db)}
+  end
+
+  def self.numbers
+    1.upto(Gtk3App::CONFIG[:SLOTS]){|n|yield(n.to_s)}
+  end
+
+  def self.get
+    alive = Sys::ProcTable.ps.select{|p|p.cmdline=~/\bruby\b.*\bgtk3app\b/}.map{|p|p.pid.to_s}.is(true)
+    Slot.dbm do |db|
+      Slot.numbers do |slot|
+        unless alive[db[slot]]
+          db[slot]=$$.to_s
+          return slot.to_i
+        end
+      end
+    end
+    return nil
+  end
+
+  def self.release(slot)
+    Slot.dbm{|db|db.delete(slot.to_s)}
+  end
+
+end
+end
