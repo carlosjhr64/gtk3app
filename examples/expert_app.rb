ee8dfe55e0c0b3b4fb45951cceae10f7ec4d739e
carlosjhr64 <carlosjhr64@gmail.com>
2017-11-27 14:43:48 -0800

    all test pass

--- examples/expert_app.rb
+++ examples/expert_app.rb
@@ -5,7 +5,7 @@ require 'gtk3app'
 module ExpertApp
   using Rafini::String
 
-  VERSION = '2.0.0'
+  VERSION = '2.0.1'
   OPTIONS = HelpParser[VERSION, <<-HELP]
 
 This is ExpertApp, an example module for Gtk3App.
