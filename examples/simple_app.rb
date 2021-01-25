48260a911c091616866892b282febff51967da97
carlosjhr64 <carlosjhr64@gmail.com>
2021-01-25 07:48:48 -0800

    simple_app not so simple

--- examples/simple_app.rb
+++ examples/simple_app.rb
@@ -1,5 +1,18 @@
 require 'gtk3app'
 
+# Gtk3App uses HelpParser.
+# If you provide ::HELP and ::VERSION,
+# Gtk3App will use them.
+HELP = <<HELP
+Usage:
+  #{File.basename $0} [:options+]
+Options:
+  --notoggle    \tMinime wont toggle decorated and keep above
+  --notdecorated\tDont decorate window
+  --notfour     \tDont show button four
+HELP
+VERSION = '1.0.0'
+
 # Using the "I don't care" `_` variable to temporarily hold
 # which's `espeak` path to test if it's there to give to the ESPEAK constant.
 ESPEAK = ((_=`which espeak 2> /dev/null`.strip) and (_!='') ? _ : nil)
@@ -41,13 +54,23 @@ end
 # with your own config:
 config = {
   main: { # Main Window
-    set_title: 'My Application',
+    set_title: 'My App',
     set_window_position: :center,
-  }
+  },
+  about_dialog: {
+    set_program_name: 'My App',
+    set_version: VERSION,
+    set_copyright: '(c) 2021 Me!',
+    set_comments: 'A very useful app by me.',
+   #set_website: '',
+   #set_website_label: '',
+  },
 }
+
 # Just pass your own config to `run`:
-Gtk3App.run(config:config) do |stage, toolbar|
+Gtk3App.run(config:config) do |stage, toolbar, options|
   hbox = Such::Box.new toolbar, [:horizontal]
+  # Such::Widget is a wrap on Gtk::Widget that automates many things...
   Such::Button.new hbox, [label: 'Button #1'] do
     says "You've pressed button number 1!"
   end
@@ -58,9 +81,12 @@ Gtk3App.run(config:config) do |stage, toolbar|
   Such::Button.new vbox, [label: 'Button #3'] do
     says "You've pressed button number 3!"
   end
-  Such::Button.new vbox, [label: 'Button #4'] do
-    says "You've pressed button number 4!"
+  unless options.notfour
+    Such::Button.new vbox, [label: 'Button #4'] do
+      says "You've pressed button number 4!"
+    end
   end
 end
 
+# Ensure a clean exit(0)
 exit
